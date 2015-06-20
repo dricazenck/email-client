988bc0c4d766d6ac354940739a119c0225a4811f
Adriana Maria Zencke Zimmermann <dricazenck@gmail.com>
2015-06-20 09:32:45 -0300

    Changed from a single html page for partial pages

--- src/javascript/email-client.js
+++ src/javascript/email-client.js
@@ -1,11 +1,23 @@
 var emailClient = (function(jQuery) {
 
-    var URL_SERVER = "/email-client/assets/emails.json";
-    var emails = [];
-    var emailList = jQuery("#email-list");
+    var URL_SERVER = "/email-client/assets/emails.json", 
+        PATH_PARTIALS = "assets/partials/";
 
+    var emails = [];
+    
     var init = function() {
+        loadInitPagesComponents();
         loadEmails();
+    };
+
+    var loadInitPagesComponents = function() {
+        loadPage("#menu", "menu.html");
+        loadPage("#emails", "emails.html");
+        loadPage("#email", "view_empty.html");
+    };
+
+    var loadPage = function(content, pageName) {
+        jQuery(content).load(PATH_PARTIALS + pageName);
     };
 
     var getEmails = function(callback) {
@@ -40,8 +52,9 @@ var emailClient = (function(jQuery) {
     var loadEmails = function() {
 
         getEmails(function(emails) {
-            emailList.html(buildEmailList(emails));
+            jQuery("#email-list").html(buildEmailList(emails));
         });
+
     };
 
     return {
