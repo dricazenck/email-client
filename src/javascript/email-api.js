b6bcfc2165b893992caf15d3502567bd4e46810e
Adriana Maria Zencke Zimmermann <dricazenck@gmail.com>
2015-06-20 16:06:38 -0300

    Added format and group by date

--- src/javascript/email-api.js
+++ src/javascript/email-api.js
@@ -2,7 +2,9 @@ var emailApi = (function(jQuery) {
 
     var URL_SERVER = "/email-client/assets/emails.json",
         RESULT_HTML = "<li><div class='from-name'>FROM_NAME</div><div class='time-ago'>TIME_AGO</div><div class='subject'>SUBJECT</div></li>",
-        SEPARATOR = "-";
+        RESULT_DATE_HTML = "<li class='email-date'>F_DATE</li>",
+        SEPARATOR = "-",
+        EMPTY_VALUE = [];
 
     var emails = [];
 
@@ -15,56 +17,84 @@ var emailApi = (function(jQuery) {
                 callback(results);
             },
             fail: function() {
-                callback([]);
+                callback(EMPTY_VALUE);
             }
         });
     };
 
-    var sortByDate = function(emails, type) {
-        var emailSorted;
+    var sortByDate = function(result, type) {
+        if (result) {
+            
+            if (typeof type === 'undefined' || type === 'DESC') {
+                return result.sort(function(email1, email2) {
+                    return email2.dateReceived - email1.dateReceived;
+                });
+            } else {
+                return result.sort(function(email1, email2) {
+                    return email1.dateReceived - email2.dateReceived;
+                });
+            }
 
-        if (typeof type === 'undefined' || type === 'DESC') {
-            emailSorted = emails.sort(function(email1, email2) {
-                return email2.dateReceived - email1.dateReceived;
-            });
         } else {
-            emailSorted =emails.sort(function(email1, email2) {
-                return email1.dateReceived - email2.dateReceived;
-            });
+            result = EMPTY_VALUE;
         }
-
-        return emailSorted;
     };
 
-    var toEmailList = function(emailRequest) {
-        var templateEmails = "<li><div class='from-name'>FROM_NAME</div><div class='time-ago'>TIME_AGO</div><div class='subject'>SUBJECT</div></li>";
-        var result = "";
+    var groupByDate = function(result) {
+        result = sortByDate(result);
+
+        var dateGroup;
         var index = "";
 
-        for (index in emailRequest) {
-            result += emailToHtml(emailRequest[index]);
+        for (index in result) {
+            result[index].formattedDate = formatDate(result[index].dateReceived);
+
+            if (dateGroup !== result[index].formattedDate) {
+                dateGroup = result[index].formattedDate;
+                result[index].grouped = true;
+            }
         }
 
         return result;
     };
 
-    var formatDate = function formattedDate(time) {
+    var formatDate = function(time) {
         return new Date(time).toISOString().substr(0, 10).split(SEPARATOR).reverse().join(SEPARATOR);
     };
 
+    var toEmailList = function(emails) {
+        emails = groupByDate(emails);
+
+        var result = "";
+        var index = "";
+
+        for (index in emails) {
+            result += emailToHtml(emails[index]);
+        }
+
+        return result;
+    };
+
     var emailToHtml = function(emailItem) {
-
-        return RESULT_HTML
-            .replace("FROM_NAME", emailItem.fromName)
-            .replace("TIME_AGO", formatDate(emailItem.dateReceived))
-            .replace("SUBJECT", emailItem.subject);
-
+        if (typeof emailItem.grouped === 'undefined') {
+            return RESULT_HTML
+                .replace("FROM_NAME", emailItem.fromName)
+                .replace("TIME_AGO", formatDate(emailItem.dateReceived))
+                .replace("SUBJECT", emailItem.subject);
+        } else {
+            return RESULT_HTML
+                .replace("FROM_NAME", emailItem.fromName)
+                .replace("TIME_AGO", formatDate(emailItem.dateReceived))
+                .replace("SUBJECT", emailItem.subject)+
+                RESULT_DATE_HTML.replace("F_DATE", emailItem.formattedDate);
+        }
     };
 
     return {
         getEmails: getEmails,
         buildList: toEmailList,
-        sortByDate: sortByDate
+        sortByDate: sortByDate,
+        groupByDate: groupByDate
     };
 
 }(jQuery));
