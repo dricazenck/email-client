2b0a9d754a43af8b3e924f640165b22c8f92b3c0
Adriana Maria Zencke Zimmermann <dricazenck@gmail.com>
2015-06-20 01:24:58 -0300

    Changed jQuery.getJSON to jQuery.ajax

--- src/javascript/email-client.js
+++ src/javascript/email-client.js
@@ -9,26 +9,44 @@ var emailClient = (function(jQuery) {
     };
 
     var getEmails = function(callback) {
-        jQuery.getJSON(URL_SERVER, function(emails) {
-                callback(emails);
-            })
-            .fail(function() {
+        jQuery.ajax({
+            type: 'GET',
+            url: URL_SERVER,
+            dataType: 'json',
+            success: function(results) {
+                callback(results);
+            },
+            fail: function() {
                 callback([]);
-            });
+            }
+        });
+    };
+
+    var buildEmailList = function(emails) {
+        var templateEmails = "<li><div class='from-name'>FROM_NAME</div><div class='time-ago'>TIME_AGO</div><div class='subject'>SUBJECT</div></li>";
+        var result = "";
+        var index = "";
+
+        for (index in emails) {
+            result += templateEmails
+                .replace("FROM_NAME", emails[index].fromName)
+                .replace("TIME_AGO", emails[index].dateReceived)
+                .replace("SUBJECT", emails[index].subject);
+        }
+
+        return result;
     };
 
     var loadEmails = function() {
-        var templateEmails = "<li><div>FROM_NAME</div><div>TIME_AGO</div><div>SUBJECT</div></li>";
-        
-        getEmails(function(emails) {
-            emailList.html("<div>E-mails:" + emails.length + "</div>");
-        });
 
-        return emails;
+        getEmails(function(emails) {
+            emailList.html(buildEmailList(emails));
+        });
     };
 
     return {
-        init: init
+        init: init,
+        buildList: buildEmailList
     };
 
 }(jQuery));
