c260d2538e0f4083dd6f355400195bc84b3eaab2
Adriana Maria Zencke Zimmermann <dricazenck@gmail.com>
2015-06-19 23:23:44 -0300

    Added simple test jasmine-jquery

--- test/spec/emailClientTest.js
+++ test/spec/emailClientTest.js
@@ -1,6 +1,17 @@
+describe("default test initial config values", function() {
+	jasmine.getFixtures().fixturesPath = 'spec/fixtures/';
+
+    it("should set 'jasmine-fixtures' as the default container id", function() {
+        expect(jasmine.getFixtures().containerId).toEqual('jasmine-fixtures');
+    });
+
+    it("should set spec/fixtures/ as the default fixtures path", function() {
+        expect(jasmine.getFixtures().fixturesPath).toEqual('spec/fixtures/');
+    });
+});
+
 describe('Email Client Spec Modules', function() {
-
-
+	
 	it('Should init emailClient', function() {
 		expect(emailClient).toBeDefined();
 	});
