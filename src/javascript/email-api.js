4661fd085c975cf3d1db3f5ca47e6f76e7c6ed7e
Adriana Maria Zencke Zimmermann <dricazenck@gmail.com>
2015-06-20 13:52:59 -0300

    Added sort by date and refactore buildList

--- src/javascript/email-api.js
+++ src/javascript/email-api.js
@@ -1,9 +1,11 @@
 var emailApi = (function(jQuery) {
 
-    var URL_SERVER = "/email-client/assets/emails.json";
+    var URL_SERVER = "/email-client/assets/emails.json",
+        RESULT_HTML = "<li><div class='from-name'>FROM_NAME</div><div class='time-ago'>TIME_AGO</div><div class='subject'>SUBJECT</div></li>",
+        SEPARATOR = "-";
 
     var emails = [];
-    
+
     var getEmails = function(callback) {
         jQuery.ajax({
             type: 'GET',
@@ -18,8 +20,51 @@ var emailApi = (function(jQuery) {
         });
     };
 
+    var sortByDate = function(emails, type) {
+        var emailSorted;
+
+        if (typeof type === 'undefined' || type === 'DESC') {
+            emailSorted = emails.sort(function(email1, email2) {
+                return email2.dateReceived - email1.dateReceived;
+            });
+        } else {
+            emailSorted =emails.sort(function(email1, email2) {
+                return email1.dateReceived - email2.dateReceived;
+            });
+        }
+
+        return emailSorted;
+    };
+
+    var toEmailList = function(emailRequest) {
+        var templateEmails = "<li><div class='from-name'>FROM_NAME</div><div class='time-ago'>TIME_AGO</div><div class='subject'>SUBJECT</div></li>";
+        var result = "";
+        var index = "";
+
+        for (index in emailRequest) {
+            result += emailToHtml(emailRequest[index]);
+        }
+
+        return result;
+    };
+
+    var formatDate = function formattedDate(time) {
+        return new Date(time).toISOString().substr(0, 10).split(SEPARATOR).reverse().join(SEPARATOR);
+    };
+
+    var emailToHtml = function(emailItem) {
+
+        return RESULT_HTML
+            .replace("FROM_NAME", emailItem.fromName)
+            .replace("TIME_AGO", formatDate(emailItem.dateReceived))
+            .replace("SUBJECT", emailItem.subject);
+
+    };
+
     return {
-        getEmails: getEmails
+        getEmails: getEmails,
+        buildList: toEmailList,
+        sortByDate: sortByDate
     };
 
 }(jQuery));
--- test/spec/email-api-test.js
+++ test/spec/email-api-test.js
@@ -3,7 +3,7 @@ var jsonValueOk = [{
     "subject": "aliquip commodo ex nostrud anim ullamco",
     "fromEmail": "paulette_parker@gmail.com",
     "fromName": "Paulette Parker",
-    "dateReceived": 1433688138,
+    "dateReceived": 1434813798485,
     "index": 0,
     "read": false,
     "_id": "558070eecadb84b2cd4d5ee2"
@@ -12,13 +12,13 @@ var jsonValueOk = [{
     "subject": "ullamco anim duis minim fugiat nostrud",
     "fromEmail": "britney_swanson@gmail.com",
     "fromName": "Britney Swanson",
-    "dateReceived": 1434247931,
+    "dateReceived": 1435052070000,
     "index": 1,
     "read": false,
     "_id": "558070ee57b34805dcfeaf07"
-}];
+}, ];
 
-describe('Email Client Modules - Build Email List', function() {
+describe('tests for getEmails request', function() {
 
     beforeEach(function() {
         jasmine.Ajax.install();
@@ -54,5 +54,62 @@ describe('Email Client Modules - Build Email List', function() {
         request = jasmine.Ajax.requests.mostRecent();
         expect(request.url).toBe("/email-client/assets/emails.json");
         expect(request.method).toBe('GET');
+    });
+
+});
+
+describe('Email Client Modules - Build Email List', function() {
+
+    it('Should is buildEmailList correctly', function() {
+        var result = emailApi.buildList(jsonValueOk);
+
+        expect(result).toContain("<li><div class='from-name'>Paulette Parker</div>");
+        expect(result).toContain("<div class='time-ago'>20-06-2015</div>");
+        expect(result).toContain("<div class='subject'>aliquip commodo ex nostrud anim ullamco</div>");
+        expect(result).toContain("<li><div class='from-name'>Britney Swanson</div>");
+        expect(result).toContain("<div class='time-ago'>23-06-2015</div>");
+        expect(result).toContain("<div class='subject'>ullamco anim duis minim fugiat nostrud</div></li>");
+    });
+
+    it('Should is buildEmailList empty', function() {
+        var result = emailApi.buildList([]);
+        expect(result).toContainText("");
+
+        result = emailApi.buildList();
+        expect(result).toContainText("");
+
+        result = emailApi.buildList(undefined);
+        expect(result).toContainText("");
+    });
+
+});
+
+describe('Email Api - Sort By Date', function() {
+    var list = [{
+        "dateReceived": 1435052070000,
+        "data": "23/06/2015"
+    }, {
+        "dateReceived": 1434327777,
+        "data": "17/01/1970"
+    }, {
+        "dateReceived": 1434061147,
+        "data": "17/01/1970"
+    }, {
+        "dateReceived": 1435138470000,
+        "data": "24/06/2015"
+    }];
+
+    it('Should is order by Date desc', function() {
+        var result = emailApi.sortByDate(list);
+
+        expect(result[0].data).toContain("24/06/2015");
+        expect(result[1].data).toContain("23/06/2015");
+        expect(result[2].data).toContain("17/01/1970");
+
+        result = emailApi.sortByDate(list, 'DESC');
+
+        expect(result[0].data).toContain("24/06/2015");
+        expect(result[1].data).toContain("23/06/2015");
+        expect(result[2].data).toContain("17/01/1970");
     });
 });
