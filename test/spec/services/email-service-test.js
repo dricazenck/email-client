9284b826665068ee4386c7923f3d51a2b7356ad7
Adriana Maria Zencke Zimmermann <dricazenck@gmail.com>
2015-06-20 19:20:50 -0300

    Validation "TIME_AGO" on template

--- test/spec/services/email-service-test.js
+++ test/spec/services/email-service-test.js
@@ -1,6 +1,8 @@
+var dateNow = new Date().getTime();
+
 var jsonValueOk = [{
     "content": "Laboris ea eiusmod consectetur .\r\n",
-    "subject": "aliquip commodo ex nostrud anim ullamco",
+    "subject": "Sub1",
     "fromEmail": "paulette_parker@gmail.com",
     "fromName": "Paulette Parker",
     "dateReceived": 1434813798485,
@@ -9,7 +11,7 @@ var jsonValueOk = [{
     "_id": "558070eecadb84b2cd4d5ee2"
 }, {
     "content": "Fugiat amet dolore enim magna.\r\n",
-    "subject": "ullamco anim duis minim fugiat nostrud",
+    "subject": "Sub2",
     "fromEmail": "britney_swanson@gmail.com",
     "fromName": "Britney Swanson",
     "dateReceived": 1435052070000,
@@ -21,7 +23,7 @@ var jsonValueOk = [{
     "subject": "Subject3",
     "fromEmail": "fromEmail3@gmail.com",
     "fromName": "fromName3",
-    "dateReceived": 1435052070000,
+    "dateReceived": dateNow,
     "index": 2,
     "read": true,
     "_id": "558070ee"
@@ -73,15 +75,20 @@ describe('Email Client Modules - Build Email List', function() {
         var result = emailApi.buildList(jsonValueOk);
 
         expect(result).toContain("<li><div class='from-name'>Paulette Parker</div>");
-        expect(result).toContain("<div class='subject'>aliquip commodo ex nostrud anim ullamco</div>");
+        expect(result).toContain("<div class='subject'>Sub1</div>");
         expect(result).toContain("<li class='email-date'>23-06-2015</li>");
 
         expect(result).toContain("<li><div class='from-name'>Britney Swanson</div>");
-        expect(result).toContain("<div class='subject'>ullamco anim duis minim fugiat nostrud</div></li>");
+        expect(result).toContain("<div class='subject'>Sub2</div></li>");
         expect(result).toContain("<li class='email-date'>20-06-2015</li>");
 
-        expect(result).toBe("<li><div class='from-name'>Britney Swanson</div><div class='time-ago'>23-06-2015</div><div class='subject'>ullamco anim duis minim fugiat nostrud</div></li><li class='email-date'>23-06-2015</li><li><div class='from-name'>fromName3</div><div class='time-ago'>23-06-2015</div><div class='subject'>Subject3</div></li><li><div class='from-name'>Paulette Parker</div><div class='time-ago'>20-06-2015</div><div class='subject'>aliquip commodo ex nostrud anim ullamco</div></li><li class='email-date'>20-06-2015</li>");
+        expect(result).toContain("<li><div class='from-name'>fromName3</div>");
+        expect(result).toContain("<div class='subject'>Subject3</div></li>");
+        expect(result).toContain("<li class='email-date'>20-06-2015</li>");
 
+        expect(result).toContain("ago</div>");
+        expect(result).toContain("<div class='time-ago'>now</div>");
+        expect(result).not.toContain("<div class='time-ago'>0 seconds ago</div>");
     });
 
     it('Should is buildEmailList empty', function() {
