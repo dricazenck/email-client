a3301b7e1c88da23f40734d1818abe55deaab6d9
Adriana Maria Zencke Zimmermann <dricazenck@gmail.com>
2015-06-20 01:25:35 -0300

    Added test to buildList

--- test/spec/emailClientTest.js
+++ test/spec/emailClientTest.js
@@ -1,5 +1,5 @@
 describe("default test initial config values", function() {
-	jasmine.getFixtures().fixturesPath = 'spec/fixtures/';
+    jasmine.getFixtures().fixturesPath = 'spec/fixtures/';
 
     it("should set 'jasmine-fixtures' as the default container id", function() {
         expect(jasmine.getFixtures().containerId).toEqual('jasmine-fixtures');
@@ -10,10 +10,68 @@ describe("default test initial config values", function() {
     });
 });
 
-describe('Email Client Spec Modules', function() {
-	
-	it('Should init emailClient', function() {
-		expect(emailClient).toBeDefined();
-	});
+var jsonValueOk = [{
+    "content": "Laboris ea eiusmod consectetur .\r\n",
+    "subject": "aliquip commodo ex nostrud anim ullamco",
+    "fromEmail": "paulette_parker@gmail.com",
+    "fromName": "Paulette Parker",
+    "dateReceived": 1433688138,
+    "index": 0,
+    "read": false,
+    "_id": "558070eecadb84b2cd4d5ee2"
+}, {
+    "content": "Fugiat amet dolore enim magna.\r\n",
+    "subject": "ullamco anim duis minim fugiat nostrud",
+    "fromEmail": "britney_swanson@gmail.com",
+    "fromName": "Britney Swanson",
+    "dateReceived": 1434247931,
+    "index": 1,
+    "read": false,
+    "_id": "558070ee57b34805dcfeaf07"
+}];
 
+describe('Email Client Modules - Load', function() {
+
+    it('Should is toBeDefined emailClient', function() {
+        expect(emailClient).toBeDefined();
+
+        emailClient.init();
+
+        var isItDone = false;
+        $.ajax('emails.json').fail(function() {
+            console.log("xuxu");
+            isItDone = true;
+        });
+
+        setTimeout(function() {
+            expect(isItDone).toBeTrue();
+            done(); 
+        }, 500);
+        
+        expect(true).toEqual(true);
+
+    });
+});
+
+describe('Email Client Modules - Build Email List', function() {
+
+    it('Should is buildEmailList correctly', function() {
+        var result = emailClient.buildList(jsonValueOk);
+
+        expect(result).toContain("Paulette Parker</div>");
+        expect(result).toContain("1433688138</div>");
+        expect(result).toContain("Britney Swanson</div>");
+        expect(result).toContain("1434247931</div>");
+    });
+
+    it('Should is buildEmailList empty', function() {
+        var result = emailClient.buildList([]);
+        expect(result).toContainText("");
+
+        result = emailClient.buildList();
+        expect(result).toContainText("");
+
+        result = emailClient.buildList(undefined);
+        expect(result).toContainText("");
+    });
 });
