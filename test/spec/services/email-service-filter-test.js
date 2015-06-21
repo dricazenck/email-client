c1a7058bc676bf03d3ce41cad63dc47e9f5de1c3
Adriana Maria Zencke Zimmermann <dricazenck@gmail.com>
2015-06-21 11:33:03 -0300

    Changed to contentById return just a single result

--- test/spec/services/email-service-filter-test.js
+++ test/spec/services/email-service-filter-test.js
@@ -1,18 +1,17 @@
-var jsonEmail = [{
-    "index": 0,
-    "read": false,
-    "_id": "558070eecadb84b2cd4d5ee2"
-}, {
-    "index": 1,
-    "read": false,
-    "_id": "558070ee57b34805dcfeaf07"
-}, {
-    "index": 2,
-    "read": true,
-    "_id": "558070ee"
-}];
-
 describe('Email Service Filter validations', function() {
+    var jsonEmail = [{
+        "index": 0,
+        "read": false,
+        "_id": "558070eecadb84b2cd4d5ee2"
+    }, {
+        "index": 1,
+        "read": false,
+        "_id": "558070ee57b34805dcfeaf07"
+    }, {
+        "index": 2,
+        "read": true,
+        "_id": "558070ee"
+    }];
 
     it('Should not filter whithout attributes to filter', function() {
         expect(emailService.filter(jsonEmail)).toBe(jsonEmail);
@@ -37,20 +36,33 @@ describe('Email Service Filter validations', function() {
 });
 
 describe('Email Service contentById validations', function() {
-
+    var jsonEmail = [{
+        "index": 0,
+        "read": false,
+        "_id": "558070eecadb84b2cd4d5ee2"
+    }, {
+        "index": 1,
+        "read": false,
+        "_id": "558070ee57b34805dcfeaf07"
+    }, {
+        "index": 2,
+        "read": true,
+        "_id": "558070ee"
+    }];
+    
     it('Should filter by read emails', function() {
         var result = emailService.contentById(jsonEmail, "558070ee");
-        expect(result.length).toBe(1);
-        expect(result[0]._id).toBe("558070ee");
-        expect(result[0].read).toBe(true);
-        expect(result[0].index).toBe(2);
+        expect(result).toBeDefined();
+        expect(result._id).toBe("558070ee");
+        expect(result.read).toBe(true);
+        expect(result.index).toBe(2);
     });
 
     it('Should filter by unread emails', function() {
         var result = emailService.contentById(jsonEmail, "558070eecadb84b2cd4d5ee2");
-        expect(result.length).toBe(1);
-        expect(result[0]._id).toBe("558070eecadb84b2cd4d5ee2");
-        expect(result[0].read).toBe(false);
-        expect(result[0].index).toBe(0);
+        expect(result).toBeDefined();
+        expect(result._id).toBe("558070eecadb84b2cd4d5ee2");
+        expect(result.read).toBe(false);
+        expect(result.index).toBe(0);
     });
 });
